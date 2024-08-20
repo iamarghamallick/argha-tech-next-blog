8526e98c5b4b71de1124e5fb0e6e38306bfecdad
Argha Mallick <arghamallick5@gmail.com>
2024-08-20 19:29:11 +0530

    HelloArgha link added

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,10 @@ export default function Home() {
         <link rel="shortcut icon" href="logo.png" type="image/png" />
       </Head>
       <div className="w-full flex flex-col items-center justify-center text-3xl gap-2 min-h-screen">
-        <p className='text-2xl md:text-3xl'>Welcome to HelloArgha</p>
-        <Link href="/blog" className='text-xl bg-slate-300 dark:bg-slate-700 hover:opacity-95 px-2 py-1 rounded-lg'>
-          <button type="button">Explore Blog</button>
+        <p className='text-2xl md:text-3xl'>Welcome to ArghaTech</p>
+        <p className='text-sm'>A open source edition of my original production deployment, HelloArgha.</p>
+        <Link href="https://helloargha.vercel.app/" className='text-xl bg-slate-300 dark:bg-slate-700 hover:opacity-95 px-2 py-1 rounded-lg'>
+          <button type="button">Visit HelloArgha</button>
         </Link>
       </div>
     </>
